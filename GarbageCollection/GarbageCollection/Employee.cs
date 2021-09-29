6744a2380717b9d37cb55c86ec1371c0921c65d8
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-09-29 12:14:51 +0530

    Push new Project

---
+++ GarbageCollection/GarbageCollection/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace GarbageCollection
+{
+    class Employee:IDisposable
+    {
+        public Employee()
+        {
+
+        }
+
+        //  Destructor also called Finalize method.
+        ~Employee()
+        {
+            GC.Collect();
+            Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            Debug.WriteLine("Destructor Invoked");
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ GarbageCollection/GarbageCollection/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace GarbageCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+
+            using (Employee employee = new Employee())
+            {
+
+            }
+                Console.ReadLine();
+        }
+    }
+}
